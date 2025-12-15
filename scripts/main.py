68c458f2fe6a326e4d5bfe29dd8ef4a9612954e4
oddman43 <albert.sevilleja@gmail.com>
2025-12-15 17:56:18 +0100

    wip

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,4 @@
-from create_doc import create_new_document, write_new_doc
+from create_doc import create_new_document
 from approvals import approve_document
 from revise_doc import revise_doc
 from config import db_path
@@ -6,8 +6,7 @@ from datetime import datetime
 
 
 if __name__ == "__main__":
-    doc, ver = create_new_document("test1", "SOP", "charlie_eng", db_path)
-    write_new_doc(doc, ver, db_path)
+    create_new_document("test1", "SOP", "charlie_eng", db_path)
     approve_document("charlie_eng", "SOP-001", db_path)
     approve_document("alice_qa", "SOP-001", db_path, datetime.now().isoformat())
     # revise_doc("alice_qa", "SOP-001", db_path)
