4ca0c56ee44184ff26a88e57961d80ed8c650f14
oddman43 <albert.sevilleja@gmail.com>
2025-12-15 16:31:52 +0100

    removed fns

--- scripts/config.py
+++ scripts/config.py
@@ -1,10 +1,5 @@
 from pathlib import Path
-import sqlite3
 import os
-from datetime import datetime
-import json
-import hashlib
-from doc_class import Document_Header, Document_Version
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -63,82 +58,3 @@ template_map: dict[str, str] = {
     "LBL": str(BASE_DIR / "storage/mock_external/Mock_Label.jpg"),
     "EXT": str(BASE_DIR / "storage/mock_external/Mock_Standard.pdf"),
 }
-
-
-def user_info(user_name: str, db_path: str) -> list:
-    with sqlite3.connect(db_path) as db:
-        cur: sqlite3.Cursor = db.cursor()
-        cur.execute(
-            "SELECT user_id, active_flag FROM users WHERE user_name = ?", (user_name,)
-        )
-        result: tuple = cur.fetchone()
-        user_id: int
-        active_flag: int
-        user_id, active_flag = result
-        cur.execute(
-            """
-            SELECT r.role_name 
-            FROM roles r 
-            JOIN users_roles ur ON r.role_id = ur.role 
-            WHERE ur.user = ?
-        """,
-            (user_id,),
-        )
-        user_roles: list[str] = [i[0] for i in cur.fetchall()]
-        return [user_id, active_flag, user_roles]
-
-
-def audit_log_docs(
-    old_object: Document_Header | Document_Version | None,
-    new_object: Document_Header | Document_Version,
-    user_id: int,
-    action: str,
-    db_path: str,
-) -> None:
-    if isinstance(new_object, Document_Header):
-        table_affected: str = "documents"
-    else:
-        table_affected: str = "versions"
-    if not old_object:
-        old_dict: dict = {}
-    else:
-        old_dict = dict(old_object)
-    new_dict: dict = dict(new_object)
-    changed_keys: list = [k for k, v in new_dict.items() if v != old_dict.get(k)]
-    old_val: dict = {k: old_dict.get(k) for k in changed_keys}
-    new_val: dict = {k: new_dict.get(k) for k in changed_keys}
-    old_val_json: str = json.dumps(old_val)
-    new_val_json: str = json.dumps(new_val)
-    record_id: int = new_object.id
-    timestam: str = datetime.now().isoformat()
-    raw_hash: str = f"{table_affected}{record_id}{user_id}{action}{old_val_json}{new_val_json}{timestam}"
-    row_hash: str = hashlib.sha256(raw_hash.encode("utf-8")).hexdigest()
-    query_insert: str = """
-        insert into audit_log (table_affected, record_id, user, action, old_val, new_val, timestamp, hash)
-        values (?, ?, ?, ?, ?, ?, ?, ?)
-        """
-    with sqlite3.connect(db_path) as db:
-        try:
-            db.execute(
-                query_insert,
-                (
-                    table_affected,
-                    record_id,
-                    user_id,
-                    action,
-                    old_val_json,
-                    new_val_json,
-                    timestam,
-                    row_hash,
-                ),
-            )
-            db.commit()
-        except sqlite3.Error as e:
-            db.rollback()
-            raise e
-
-
-def doc_info(doc_name: str, db_path: str) -> Document_Version: ...
-
-
-def version_info(doc_id: int, db_path: str) -> Document_Header: ...
